3047199306c346b9b40e6939872096ccf9914c1f
asish-tailwind <asish.s@mindfiresolutions.com>
2024-09-01 23:23:13 +0530

    test 7

--- startup.sh
+++ startup.sh
@@ -4,7 +4,7 @@
 eval $(printenv | sed -n "s/^\([^=]\+\)=\(.*\)$/export \1=\2/p" | sed 's/"/\\\"/g' | sed '/=/s//="/' | sed 's/$/"/') >> /etc/profile
 
 # Print the IDENTITY_ENDPOINT environment variable to the log
-echo "IDENTITY_ENDPOINT: $IDENTITY_ENDPOINT"
+echo "IDENTITY_ENDPOINT =>: $IDENTITY_ENDPOINT"
 
 # Execute the original command (start your application)
 exec "$@"
